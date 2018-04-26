f867b183067671f28c5932e6a0d569a0243860dc
Vlad Lazar <vladlazar94@outlook.com>
2018-04-26 22:48:19 +0300

    Problem_5 solution

---
+++ Problem_5/main.cpp
@@ -0,0 +1,60 @@
+// Problem_5: Write a program that checks whether two given strings are one edit away from eachother.
+// Edits include inserting, removing or replacing a character.
+
+#include <iostream>
+#include <string>
+
+// O(n) in the size of the shorter string.
+bool oneEditAway(const std::string& first, const std::string& second){
+    if((first.size() - second.size()) > 1){
+        return false;
+    }
+
+    bool oneEditAlready = false;
+    if(first.size() == second.size()){
+        for(int index = 0; index < first.size(); index++){
+            if(first[index] != second[index]){
+                if(oneEditAlready){
+                    return false;
+                }
+                oneEditAlready = true;
+            }
+        }
+        return true;
+    }
+    else{
+        // Get the shorter, respectively the longer string:
+        const std::string& longerString = first.size() > second.size() ? first : second;
+        const std::string& shorterString = first.size() > second.size() ? second : first;
+
+        // Loop through the string with different indices:
+        size_t longerIndex = 0;
+        size_t shorterIndex = 0;
+
+        for(;shorterIndex < shorterString.size() && longerIndex < longerString.size();){
+
+            if(shorterString[shorterIndex] != longerString[longerIndex]){
+                longerIndex++;
+                if(longerIndex - shorterIndex > 1){
+                    return false;
+                }
+            }
+
+            longerIndex++;
+            shorterIndex++;
+        }
+        return true;
+    }
+}
+
+int main() {
+
+    // Tests:
+    std::cout<<oneEditAway("abc", "abd")<<std::endl; // True.
+    std::cout<<oneEditAway("abc", "ab")<<std::endl; // True.
+    std::cout<<oneEditAway("abc", "dab")<<std::endl; // False.
+    std::cout<<oneEditAway("abc", "ac")<<std::endl; // True.
+    std::cout<<oneEditAway("abc", "aef")<<std::endl; // False.
+
+    return 0;
+}

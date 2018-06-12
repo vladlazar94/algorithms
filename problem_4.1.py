23428b82281ed031e986a9fe43c3f9b5015b89fe
Vlad Lazar <vladlazar@outlook.com>
2018-06-12 19:52:00 +0100

    problem 4.1 full

--- problem_4.1.py
+++ problem_4.1.py
@@ -21,45 +21,29 @@ class Graph:
         self.nodes.append(node)
 
 def exists_route (graph, firstNode, secondNode):
-    if not firstNode in graph.nodes or not secondNode in graph.nodes: return False
-    if firstNode == secondNode: return True
-
-    firstNode = graph.nodes[graph.nodes.index(firstNode)]
-    secondNode = graph.nodes[graph.nodes.index(secondNode)]
-    
-    q = deque()
-    firstNode.visited = 'red'
-    q.appendleft(firstNode)
-    
-    while len(q) > 0:  
-        node = q.pop()
-        if node == secondNode: return True 
-        for index in node.neighbours:
-            if not graph.nodes[index].visited == 'red': 
-                graph.nodes[index].visited = 'red'
-                q.appendleft(graph.nodes[index])
-    
-    return False
-            
+    def one_way(graph, firstNode, secondNode):
         
+        if not firstNode in graph.nodes or not secondNode in graph.nodes: return False
+        if firstNode == secondNode: return True
+        graph.reset()
 
-
-
-
-
-
-nodes = []
-graph = Graph()
-nodes.append(GraphNode('A', [1]))
-nodes.append(GraphNode('B', [2, 3]))
-nodes.append(GraphNode('C', [3]))
-nodes.append(GraphNode('D', [4, 1]))
-nodes.append(GraphNode('E', [5, 0, 1]))
-nodes.append(GraphNode('F', [0]))
-nodes.append(GraphNode('G', []))
-for node in nodes:
-    graph.push(node)
-
-print(exists_route(graph, graph.nodes[3], graph.nodes[6]))
+        firstNode = graph.nodes[graph.nodes.index(firstNode)]
+        secondNode = graph.nodes[graph.nodes.index(secondNode)]
+    
+        q = deque()
+        firstNode.visited = True
+        q.appendleft(firstNode)
+    
+        while len(q) > 0:  
+            node = q.pop()
+            if node == secondNode: return True 
+            for index in node.neighbours:
+                if not graph.nodes[index].visited == True: 
+                    graph.nodes[index].visited = True
+                    q.appendleft(graph.nodes[index])
+    
+        return False
+    
+    return one_way(graph, firstNode, secondNode) or one_way(graph, secondNode, firstNode)
 
 

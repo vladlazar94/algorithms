6685abaab034409683edb388fc287fdea3e9683e
Vlad Lazar <vladlazar@outlook.com>
2018-06-14 12:21:22 +0100

    simpler BSTree

--- trees.py
+++ trees.py
@@ -1,62 +1,30 @@
-class BinaryTreeNode:
-    def __init__ (self, data):
+class BSTree:
+    def __init__ (self, data = None):
         self.data = data
         self.left = None
         self.right = None
-    
-    def get_left (self):
-        return self.left
 
-    def get_right (self):
-        return self.right
-    
-    def get_data (self):
-        return self.data
-
-    def set_left (self, left):
-        self.left = left
-
-    def set_right (self, right):
-        self.right = right
-
-    def set_data (self, data):
-        self.data = data
-
-class BinarySearchTree:
-    def __init__ (self):
-        self.root = BinaryTreeNode(None)
-
-    def set_root (self, root):
-        self.root = root
-
-    def get_root (self):
-        return self.root
-
-    def add_rec (self, node, data):
-        if data < node.get_data():
-            if node.get_left() is None:
-                node.set_left(BinaryTreeNode(data))
+    def insert (self, data):
+        if self.data is None:
+            self.data = data
+        elif data < self.data:
+            if self.left is None:
+                self.left = BSTree(data)
             else:
-                self.add_rec(node.get_left(), data)
-        elif data > node.get_data():
-            if node.get_right() is None: 
-                node.set_right(BinaryTreeNode(data))
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = BSTree(data)
             else:
-                self.add_rec(node.get_right(), data)
-        else: return
-
-    def print_inorder_rec (self, node):
-        if node is not None:
-            self.print_inorder_rec(node.get_left())
-            print(node.get_data())
-            self.print_inorder_rec(node.get_right())
-
-    def add (self, data):
-        if self.root.get_data() is None:
-            self.root.set_data(data)
+                self.right.insert(data)
         else:
-            self.add_rec(self.root, data)
+            return
+    
+    def inorder_print (self):
+        if self.left is not None:
+            self.left.inorder_print()
+        print(self.data)
+        if self.right is not None:
+            self.right.inorder_print()
 
-    def print_inorder (self):
-        self.print_inorder_rec(self.root)
 

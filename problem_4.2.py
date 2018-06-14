72b0add265d37dac53d6ae120f07f85b74504278
Vlad Lazar <vladlazar@outlook.com>
2018-06-14 13:56:55 +0100

    better solution to 4.2

--- problem_4.2.py
+++ problem_4.2.py
@@ -1,47 +1,46 @@
 # Problem 4.2: Given a sorted (increasing order) array with unique integer elements,
 # write an algorithm to create a binary search tree of minimal height.
 
-class Node:
-    def __init__ (self, data):
+class BSTree:
+    def __init__ (self, data = None):
         self.data = data
         self.left = None
-        self.right = None 
+        self.right = None
+
+    def insert (self, data):
+        if self.data is None:
+            self.data = data
+        elif data < self.data:
+            if self.left is None:
+                self.left = BSTree(data)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = BSTree(data)
+            else:
+                self.right.insert(data)
+        else:
+            return
     
-def insert_binary (root, data):
-    if root.data is None:
-        root.data = data
-    elif data < root.data:
-        if root.left is None:
-            root.left = Node(data)
-        else:
-            insert_binary(root.left, data)
-    elif data > root.data:
-        if root.right is None:
-            root.right = Node(data)
-        else:
-            insert_binary(root.right, data)
-    else: return
+    def inorder_print (self):
+        if self.left is not None:
+            self.left.inorder_print()
+        print(self.data)
+        if self.right is not None:
+            self.right.inorder_print()
 
-def print_inorder (root):
-    if root is not None:
-        print_inorder(root.left)
-        print(root.data) 
-        print_inorder(root.right)
-
-def create_bst_rec (bst, sorted_list, index_start, index_end):
-    if index_end - index_start == 1:
-        insert_binary(bst, sorted_list[index_start])
-        insert_binary(bst, sorted_list[index_end])
-    elif index_end == index_start:
-        insert_binary(bst, sorted_list[index_start])
-    else:
-        index_middle = (index_start + index_end) / 2
-        insert_binary(bst, sorted_list[index_middle])
-        create_bst_rec(bst, sorted_list, index_start, index_middle - 1)
-        create_bst_rec(bst, sorted_list, index_middle + 1, index_end)
-
-def create_balanced_bst (sorted_list):
-    bst = Node(None)
-    create_bst_rec(bst, sorted_list, 0, len(sorted_list) - 1)
+def create_bst_rec  (sorted_list, index_start, index_end):
+    if index_end < index_start:
+        return None    
+    index_middle = (index_start + index_end) // 2
+    bst = BSTree(sorted_list[index_middle])
+    bst.left = create_bst_rec(sorted_list, index_start, index_middle - 1)
+    bst.right = create_bst_rec(sorted_list, index_middle + 1, index_end)
     return bst
+
+def create_bst (sorted_list):
+    return create_bst_rec(sorted_list, 0, len(sorted_list) - 1)
+
+
 
--- problem_8.py
+++ problem_8.py
@@ -10,26 +10,18 @@ def zeroElement(matrix):
 
     for row in range (len(matrix)):
         for column in range(len(matrix)):
-
-            if matrix[row][column] == 0:
-                
+            if matrix[row][column] == 0:     
                 rowBools[row] = True
                 columnBools[column] = True
 
-    for rowIndex in range (len(rowBools)):
-        
+    for rowIndex in range (len(rowBools)):      
         if rowBools[rowIndex] == True:
-
             for columnIndex in range(len(matrix)):
-
                 matrix[rowIndex][columnIndex] = 0
 
     for columnIndex in range (len(columnBools)):
-
         if columnBools[columnIndex] == True:
-
             for rowIndex in range(len(matrix)):
-
                 matrix[rowIndex][columnIndex] = 0
 
 

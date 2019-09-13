5fba0a63c2abcb9862d62c1d2906da8340fa7828
Vlad Lazar <vladlazar94@outlook.com>
2019-09-13 16:10:49 +0200

    Re-implemented the PriorityQueue to accept empty interfaces instead of ints.

--- heap/PriorityQueue.go
+++ heap/PriorityQueue.go
@@ -4,89 +4,101 @@ package heap
 PriorityQueue description...
 */
 type PriorityQueue struct {
-	heap []int
+	Heap []QueueNode
+}
+
+/*
+QueueNode description...
+*/
+type QueueNode struct {
+	Val      interface{}
+	Priority float64
 }
 
 /*
 Empty description...
 */
 func (queue *PriorityQueue) Empty() bool {
-	return len(queue.heap) == 0
+	return len(queue.Heap) == 0
 }
 
 /*
-Push description...
+Peek description...
 */
-func (queue *PriorityQueue) Push(num int) {
-	queue.heap = append(queue.heap, num)
-	queue.siftUp(len(queue.heap) - 1)
+func (queue *PriorityQueue) Peek() (interface{}, float64) {
+	firstNode := queue.Heap[0]
+	return firstNode.Val, firstNode.Priority
 }
 
 /*
-ExtractMax description...
+Insert description...
 */
-func (queue *PriorityQueue) ExtractMax() int {
-	max := queue.heap[0]
-	lastElemIndex := queue.heap[len(queue.heap)-1]
-
-	queue.swap(0, lastElemIndex)
-	queue.heap = queue.heap[0:lastElemIndex]
-	queue.siftDown(0)
-
-	return max
+func (queue *PriorityQueue) Insert(Val interface{}, Priority float64) {
+	queue.Heap = append(queue.Heap, QueueNode{Val, Priority})
+	lastNodeIndex := len(queue.Heap) - 1
+	queue.siftUp(lastNodeIndex)
 }
 
 /*
-Peek description
+Pop description...
 */
-func (queue *PriorityQueue) Peek() int {
-	return queue.heap[0]
+func (queue *PriorityQueue) Pop() (interface{}, float64) {
+	topNode := queue.Heap[0]
+
+	lastNodeIndex := len(queue.Heap) - 1
+
+	queue.swap(0, lastNodeIndex)
+	queue.Heap = queue.Heap[0:lastNodeIndex]
+
+	if !queue.Empty() {
+		queue.siftDown(0)
+	}
+
+	return topNode.Val, topNode.Priority
 }
 
 func (queue *PriorityQueue) swap(i, j int) {
-	heap := queue.heap
-	temp := heap[i]
-	heap[i] = heap[j]
-	heap[j] = temp
+	temp := queue.Heap[i]
+	queue.Heap[i] = queue.Heap[j]
+	queue.Heap[j] = temp
 }
 
-func (queue *PriorityQueue) siftDown(i int) {
-	current := i
-	heap := queue.heap
+func (queue *PriorityQueue) siftDown(nodeIndex int) {
+	heap := queue.Heap
+	indexOfCurr := nodeIndex
 
-	for current < len(heap) {
-		left, right := leftChildIndex(current), rightChildIndex(current)
-		max := current
+	for indexOfCurr < len(heap) {
+		indexOfLeft, indexOfRight := leftChildIndex(indexOfCurr), rightChildIndex(indexOfCurr)
+		indexOfMax := indexOfCurr
 
-		if left < len(heap) && heap[left] > heap[max] {
-			max = left
+		if indexOfLeft < len(heap) && heap[indexOfLeft].Priority > heap[indexOfMax].Priority {
+			indexOfMax = indexOfLeft
 		}
 
-		if right < len(heap) && heap[right] > heap[max] {
-			max = right
+		if indexOfRight < len(heap) && heap[indexOfRight].Priority > heap[indexOfMax].Priority {
+			indexOfMax = indexOfRight
 		}
 
-		if max == current {
+		if indexOfMax == indexOfCurr {
 			return
 		}
 
-		queue.swap(current, max)
-		current = max
+		queue.swap(indexOfMax, indexOfCurr)
+		indexOfCurr = indexOfMax
 	}
 }
 
-func (queue *PriorityQueue) siftUp(i int) {
-	heap := queue.heap
-	current := i
+func (queue *PriorityQueue) siftUp(nodeIndex int) {
+	indexOfCurr := nodeIndex
 
-	for current > 0 {
-		parentIndex := parentIndex(i)
+	for indexOfCurr > 0 {
+		indexOfParent := parentIndex(indexOfCurr)
 
-		if heap[parentIndex] >= heap[current] {
+		if queue.Heap[indexOfParent].Priority >= queue.Heap[indexOfCurr].Priority {
 			return
 		}
 
-		queue.swap(current, parentIndex)
-		current = parentIndex
+		queue.swap(indexOfCurr, indexOfParent)
+		indexOfCurr = indexOfParent
 	}
 }

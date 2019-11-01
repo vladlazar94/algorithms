c800c13ea36aa385535ff207164ccd4e9afda4a2
Vlad Lazar <vladlazar94@outlook.com>
2019-11-01 13:31:36 +0100

    Added insertion sort.

---
+++ sorting/insertion.go
@@ -0,0 +1,15 @@
+package sorting
+
+func insertion(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		key := nums[i]
+
+		j := i - 1
+		for j > 0 && nums[j] > key {
+			nums[j+1] = nums[j]
+			j--
+		}
+
+		nums[j+1] = key
+	}
+}

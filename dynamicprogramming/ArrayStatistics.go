bbb1550d2d50ea87c20a5dc090494abb88847d58
Vlad Lazar <vladlazar94@outlook.com>
2020-02-01 11:11:24 +0100

    Refactored MaxSubarraySum.

---
+++ dynamicprogramming/ArrayStatistics.go
@@ -0,0 +1,29 @@
+package dynamicprogramming
+
+/*
+MaxSubarraySum returns the highest sum over all contiguous subarrays of the input array.
+Notations:
+	Solution(n) -> the solution for the first n elements of the array
+
+Sketch:
+	{ * * * * * * * * * * * * * * * * * (*) * * * * * * }
+                                         i -> current index
+
+	[           Dolution(i-1)            ]
+	   [ Best sum ]
+						[  Best sum @end ]
+
+Complexity:
+	Theta(n) time | Theta(1) space, where n is the number of numbers in the input array.
+*/
+func MaxSubarraySum(nums []int) int {
+	bestSum := 0
+	bestSumAdjacentToEnd := 0
+
+	for _, num := range nums {
+		bestSumAdjacentToEnd = max(0, num+bestSumAdjacentToEnd)
+		bestSum = max(bestSum, bestSumAdjacentToEnd)
+	}
+
+	return bestSum
+}
--- dynamicprogramming/CoinCombinations.go
+++ dynamicprogramming/CoinCombinations.go
@@ -1,5 +1,7 @@
 package dynamicprogramming
 
+import "math"
+
 /*
 CoinCombinations returns the number of linear combinations of coin values amounting to the target sum.
 Notations:
@@ -11,7 +13,7 @@ Recursion:
     Solution(target, n) = Solution(target, n-1) + Solution(target - Coins[n], n)
 
 Complexity:
-    Theta(target * n) time | Theta(target) space, where n is the number of coin values
+    O(target * n) time | Theta(target) space, where n is the number of coin values
 */
 func CoinCombinations(target int, coinValues []int) int {
 	partialSolutions := append([]int{1}, make([]int, target)...)
@@ -22,6 +24,49 @@ func CoinCombinations(target int, coinValues []int) int {
 				partialSolutions[i] += partialSolutions[i-value]
 			}
 		}
+	}
+
+	return partialSolutions[target]
+}
+
+/*
+MinCoinCombinations returns the minimum number of coins required to amount up to the target value.
+Notations:
+	Coins 			 -> the coin values
+	Coins[i] 		 -> the ith coin value
+	Solution(target) -> the minimum number of coins that added up amount to target
+
+Insights:
+	For any non-zero target, the a solution representing the smallest set of coins that add up to
+	the target must contain at least one coin. The rest of the coins comprised in the solution
+	must necessarily yield a solution to the same problem with a new target equal to the target
+	minus the value of that one coin. This allows us to relate the problem determined by the target
+	and the set of coins to a problem determined by a lower target and the same set of coins,
+	as is captured in the following recursion.
+
+Recursion:
+	Solution(target) = Max({1 + Solution(target - Coins[i]) | i from 0 to number of coin values})
+
+Complexity:
+	O(target * n) time | Theta(target) space, where n is the number of coin values
+*/
+func MinCoinCombinations(target int, coinValues []int) int {
+	partialSolutions := make([]int, target+1)
+
+	for i := 1; i < len(partialSolutions); i++ {
+		partialSolutions[i] = math.MaxInt32
+	}
+
+	for i := range partialSolutions {
+		for _, value := range coinValues {
+			if value <= i {
+				partialSolutions[i] = min(partialSolutions[i], partialSolutions[i-value]+1)
+			}
+		}
+	}
+
+	if partialSolutions[target] == math.MaxInt32 {
+		return -1
 	}
 
 	return partialSolutions[target]
